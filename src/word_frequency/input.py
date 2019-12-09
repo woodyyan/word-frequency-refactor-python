8c7828f73d7836bc9c186dadec802fc61a08dfc2
Songbai Yan <colorguitar@hotmail.com>
2019-12-09 15:16:46 +0800

    init project

---
+++ src/word_frequency/input.py
@@ -0,0 +1,4 @@
+class Input:
+    def __init__(self, value, count):
+        self.value = value
+        self.count = count
---
+++ src/word_frequency/word_freq_game.py
@@ -0,0 +1,36 @@
+from src.word_frequency.input import Input
+
+
+def count_word_frequency(paragraph: str) -> str:
+    word_list = paragraph.split()
+
+    if len(word_list) == 1:
+        return word_list[0] + " 1"
+    else:
+        input_list = []
+        for word in word_list:
+            input_list.append(Input(word, 1))
+
+        word_dict = {}
+        for input_item in input_list:
+            if input_item.value in word_dict:
+                word_dict[input_item.value].append(input)
+            else:
+                word_dict[input_item.value] = [input]
+
+        new_list = []
+        for word, value in word_dict.items():
+            count = len(word_dict[word])
+            new_list.append(Input(word, count))
+
+        sorted_list = sorted(new_list, key=lambda x: x.count, reverse=True)
+
+        result = ""
+        for item in sorted_list:
+            result += f'{item.value} {item.count} \n'
+        return result.strip()
+
+if __name__ == '__main__':
+    paragraph = "the the is"
+    result = count_word_frequency(paragraph)
+    print(result)
