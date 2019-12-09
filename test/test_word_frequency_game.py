09c56c77a680efc00b8b7be962cce067690b35cd
Songbai Yan <colorguitar@hotmail.com>
2019-12-09 16:53:34 +0800

    add test_should_return_2_the_1_is_when_count_word_frequency_given_the_the_is_with_extra_space test

--- test/test_word_frequency_game.py
+++ test/test_word_frequency_game.py
@@ -10,3 +10,11 @@ class TestWordFrequencyGame(unittest.TestCase):
     def test_should_return_1_the_1_is_when_count_word_frequency_given_the_is(self):
         word_frequency = src.word_frequency.word_freq_game.count_word_frequency('the is')
         self.assertEqual(word_frequency, 'the 1 \nis 1')
+
+    def test_should_return_empty_when_count_word_frequency_given_empty(self):
+        word_frequency = src.word_frequency.word_freq_game.count_word_frequency('')
+        self.assertEqual(word_frequency, '')
+
+    def test_should_return_2_the_1_is_when_count_word_frequency_given_the_the_is_with_extra_space(self):
+        word_frequency = src.word_frequency.word_freq_game.count_word_frequency('  the   the   is  ')
+        self.assertEqual(word_frequency, 'the 2 \nis 1')
