a5f4f7b62dc40b6e8ef8dc8760057f924b90f2d1
Songbai Yan <colorguitar@hotmail.com>
2019-12-09 16:14:53 +0800

    add test_should_return_2_the_1_is_when_count_word_frequency_given_the_the_is test

---
+++ test/test_word_frequency_game.py
@@ -0,0 +1,8 @@
+import unittest
+import src.word_frequency.word_freq_game
+
+
+class TestWordFrequencyGame(unittest.TestCase):
+    def test_should_return_2_the_1_is_when_count_word_frequency_given_the_the_is(self):
+        word_frequency = src.word_frequency.word_freq_game.count_word_frequency('the the is')
+        self.assertEqual(word_frequency, 'the 2 \nis 1')
