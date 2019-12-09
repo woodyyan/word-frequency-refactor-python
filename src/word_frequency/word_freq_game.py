40c42240199acec492b779b0d51a9f94a4621cda
Songbai Yan <colorguitar@hotmail.com>
2019-12-09 17:28:45 +0800

    extract count_words method

--- src/word_frequency/word_freq_game.py
+++ src/word_frequency/word_freq_game.py
@@ -4,25 +4,27 @@ from src.word_frequency.input import Input
 def count_word_frequency(paragraph: str) -> str:
     words = paragraph.split()
 
+    sorted_list = count_words(words)
+
+    return render_word_frequency(sorted_list)
+
+
+def count_words(words):
     input_list = []
     for word in words:
         input_list.append(Input(word, 1))
-
     word_dict = {}
     for input_item in input_list:
         if input_item.value in word_dict:
             word_dict[input_item.value].append(input)
         else:
             word_dict[input_item.value] = [input]
-
     new_list = []
     for word, value in word_dict.items():
         count = len(word_dict[word])
         new_list.append(Input(word, count))
-
     sorted_list = sorted(new_list, key=lambda x: x.count, reverse=True)
-
-    return render_word_frequency(sorted_list)
+    return sorted_list
 
 
 def render_word_frequency(sorted_list):
